4b19ce663323cb6a9f58eb931e7f48766afc4525
Eric Milas <ericmilas@gmail.com>
2019-12-29 20:34:55 -0500

    initial commit

---
+++ client/components/image_list.js
@@ -0,0 +1,15 @@
+//Create our image list component
+//Import React
+import React from 'react';
+
+//Create the component
+const ImageList = () => {
+    return(
+        <ul>
+            <li>Image Detail</li>
+        </ul>
+    );
+};
+
+//Export the component
+ export default ImageList;
---
+++ client/main.js
@@ -0,0 +1,20 @@
+//Any JS in here is automatically run for us
+
+//Import react
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ImageList from "./components/image_list";
+
+//Create a component
+const App = () => {
+    return (
+        <div>
+            <h1>Image List</h1>
+        </div>
+    );
+};
+
+//Render the component to the screen
+Meteor.startup(() => {
+    ReactDOM.render(<App/>, document.querySelector('.container'));
+});
